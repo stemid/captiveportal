b84b888cd4d6ae998892b42e7a7e7223bc4f3393
Stefan Midjich <swehack@gmail.com>
2017-09-29 19:04:06 +0200

    cursor problem

--- tools/purge_clients.py
+++ tools/purge_clients.py
@@ -31,11 +31,14 @@ config.readfp(args.config)
 
 sr = StoragePostgres(config=config)
 
-for client_id in sr.client_ids():
+# Get cursor from psycopg2
+client_ids = sr.client_ids()
+
+for client_id in client_ids:
     client = Client(
         storage=sr,
         chain=config.get('iptables', 'chain'),
-        client_id=client_id[0]
+        client_id=client_id
     )
 
     if datetime.now() > client.expires:
